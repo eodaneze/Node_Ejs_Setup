464789e47de5173eeda619b08c3c56d8cec15b65
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-01-16 04:19:30 -0800

    displaying data from backend to frontend

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from 'express'
+const express = require('express')
 
 const app = express();
 
@@ -11,6 +11,14 @@ app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
 app.use('/img', express.static(__dirname + 'public/img'));
+
+
+app.get('/', (req, res, next) => {
+    // res.sendFile(__dirname + '/views/index.html')
+    res.json({
+        message: "HELLO FROM BACKEND"
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
