3217fc58f5626e68ba59fe02ee276aa44dd06f47
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-01-16 03:53:14 -0800

    setting up the static files in the server.js

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express();
 
 const PORT = 5000
 
+
+// static files
+
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
+
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 })
