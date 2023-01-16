1dfff30a7d7d64f24e547a782588d100a554d16d
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-01-16 04:26:47 -0800

    using the ejs templating

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require('express')
+const express = require("express");
 
 const app = express();
 
-const PORT = 5000
-
+const PORT = 5000;
 
 // static files
 
-app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/js', express.static(__dirname + 'public/js'));
-app.use('/img', express.static(__dirname + 'public/img'));
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
 
+// set views
 
-app.get('/', (req, res, next) => {
-    // res.sendFile(__dirname + '/views/index.html')
-    res.json({
-        message: "HELLO FROM BACKEND"
-    })
-})
+app.set("views", "./views");
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res, next) => {
+  // res.sendFile(__dirname + '/views/index.html')
+  res.render('index', {text: "This is EJS"})
+  res.send('backend says hi')
+});
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-})
+  console.log(`server is running on port ${PORT}`);
+});
